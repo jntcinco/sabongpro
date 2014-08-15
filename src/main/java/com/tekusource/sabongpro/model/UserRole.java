20bad804897b49fd02c7b243f3c170e6bfbbda38
jntcinco <jntcinco@gmail.com>
2014-08-15 15:19:19 +0800

    fix register issue when continuously adding user with the same role

--- src/main/java/com/tekusource/sabongpro/model/UserRole.java
+++ src/main/java/com/tekusource/sabongpro/model/UserRole.java
@@ -1,13 +1,14 @@
 package com.tekusource.sabongpro.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -26,8 +27,8 @@ public class UserRole implements Serializable {
 	@Column(name="user_role")
 	private String role;
 	
-	@OneToOne(mappedBy="userRole")
-	private User user;
+	@OneToMany(mappedBy="userRole")
+	private List<User> users;
 
 	public Long getId() {
 		return id;
@@ -45,11 +46,11 @@ public class UserRole implements Serializable {
 		this.role = role;
 	}
 	
-	public User getUser() {
-		return user;
+	public List<User> getUsers() {
+		return users;
 	}
 	
-	public void setUser(User user) {
-		this.user = user;
+	public void setUsers(List<User> users) {
+		this.users = users;
 	}
 }
