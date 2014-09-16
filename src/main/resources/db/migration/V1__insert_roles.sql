fddd7051e64162befba43799448028ee08e2fa18
Kevin Jude "Gwapo" Deloria <akiwarheit@gmail.com>
2014-09-16 10:09:21 +0800

    Added flyway migrations

---
+++ src/main/resources/db/migration/V1__insert_roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO sabongpro.user_roles values (0, 'ADMIN');
+INSERT INTO sabongpro.user_roles values (1, 'GUEST');
+INSERT INTO sabongpro.users values (0, 'akiwarheit@gmail.com', 'YWRtaW4=', 'Active', 1, 'admin', null, 0);
